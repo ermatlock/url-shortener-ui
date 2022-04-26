5211304e5bfe504b464fa2d73bcff3dd48c9a1c1
Eric Matlock <ermatlock@gmail.com>
2022-04-26 12:39:26 -0400

    Add geturl's

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getUrls } from '../../apiCalls';
+import { getUrls, postUrl } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
@@ -13,8 +13,12 @@ export class App extends Component {
   }
 
   componentDidMount() {
+    getUrls().then(data => this.setState({urls: data.urls}))
   }
 
+  
+
+  
   render() {
     return (
       <main className="App">
