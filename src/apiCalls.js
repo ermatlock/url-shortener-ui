6c3a012845f1f5aa5ce1330565e7ed4efbeb2d18
Eric Matlock <ermatlock@gmail.com>
2022-04-26 12:55:24 -0400

    Add headers

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,6 +8,9 @@ export const getUrls = () => {
 export const postUrl = ({long_url, title}) => {
   return fetch('http://localhost:3001/api/v1/urls', {
     method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({
       long_url: long_url,
       title: title
