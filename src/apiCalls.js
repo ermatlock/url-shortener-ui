79c6c9cae0ce4b2a4e5768bdb9b97fb8e99acc0f
Eric Matlock <ermatlock@gmail.com>
2022-04-26 12:38:56 -0400

    Add fetch and post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,16 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+  .then(response => response.json())
+  // .then(response => console.log(response))
+  
+}
+
+export const postUrl = ({long_url, title}) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: "POST",
+    body: JSON.stringify({
+      long_url: long_url,
+      title: title
+    })
+  })
 }
